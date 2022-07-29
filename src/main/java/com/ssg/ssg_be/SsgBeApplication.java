1b6feb77740696ba7e9325d61317ddcb19594201
K-J-HYEON <kjh950601@naver.com>
2022-07-29 10:14:41 +0900

    ssg_be

---
+++ src/main/java/com/ssg/ssg_be/SsgBeApplication.java
@@ -0,0 +1,13 @@
+package com.ssg.ssg_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssg/ssg_be/SsgBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssg.ssg_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
