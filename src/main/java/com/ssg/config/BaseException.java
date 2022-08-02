b82d21e3990a851887d5bd47935f256600d7b3dd
안대현 <gch05610@gmail.com>
2022-08-02 14:52:10 +0900

    Update : BaseResponse & BaseException 기본 코드 구현

--- src/main/java/com/ssg/config/BaseException.java
+++ src/main/java/com/ssg/config/BaseException.java
@@ -1,4 +1,12 @@
 package com.ssg.config;
 
-public class BaseException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseException extends Exception {
+    private BaseResponseStatus status;
 }
--- src/main/java/com/ssg/config/BaseResponse.java
+++ src/main/java/com/ssg/config/BaseResponse.java
@@ -1,12 +1,41 @@
 package com.ssg.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import static com.ssg.config.BaseResponseStatus.SUCCESS;
+
 @Getter
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
-public class BaseResponse {
+public class BaseResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String message;
+    private final int code;
+    @JsonInclude(JsonInclude.Include.NON_NULL)  // null인 데이터는 안나오도록 함(요청 실패한 경우)
+    private T result;
+
+    // 요청 성공한 경우
+    // BaseResponseStatus에 정의된 SUCCESS 상수를 이용해서 result 객체와 함께 반환
+    public BaseResponse(T result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.message = SUCCESS.getMessage();
+        this.code = SUCCESS.getCode();
+        this.result = result;
+    }
+
+    // 요청 실패한 경우
+    // BaseResponseStatus에 정의된 상수를 받아와서 반환
+    public BaseResponse(BaseResponseStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+    }
+
 
 }
