19367145697858d1601101d1e98bb66802203cc5
안대현 <gch05610@gmail.com>
2022-08-11 17:15:56 +0900

    Feat : Review Repo, Dto 추가

---
+++ src/main/java/com/ssg/ssg_be/review/infrastructure/ReviewRepository.java
@@ -0,0 +1,18 @@
+package com.ssg.ssg_be.review.infrastructure;
+
+import com.ssg.ssg_be.review.domain.Review;
+import com.ssg.ssg_be.review.domain.ReviewTotalDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    boolean existsByProduct_ProductId(Long productId);
+
+    @Query(value = "select count(r) as reviewCount, avg(r.score) as reviewAvg from Review r where r.product.productId =:productId")
+    ReviewTotalDto retrieveReviewAvg(@Param("productId") Long productId);
+
+}
