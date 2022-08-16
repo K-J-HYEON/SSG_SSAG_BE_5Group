08eb0095c0e1480780ad42ee9baff6264fdc9ee3
안대현 <gch05610@gmail.com>
2022-08-16 15:59:57 +0900

    Delete : 판매자 관련 파일 삭제

--- src/main/java/com/ssg/ssg_be/signup/application/SellerSignupService.java
+++
@@ -1,9 +0,0 @@
-package com.ssg.ssg_be.signup.application;
-
-import com.ssg.config.BaseException;
-import com.ssg.ssg_be.signup.domain.SellerDtoReq;
-
-public interface SellerSignupService {
-    void addSeller(SellerDtoReq sellerDtoReq) throws BaseException;
-
-}
