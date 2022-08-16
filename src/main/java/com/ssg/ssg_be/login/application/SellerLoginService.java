c57062125f9c4f75243ff57d49d8db97aec46b68
안대현 <gch05610@gmail.com>
2022-08-16 14:43:35 +0900

    Delete : 판매자 로그인 API 삭제

--- src/main/java/com/ssg/ssg_be/login/application/SellerLoginService.java
+++
@@ -1,8 +0,0 @@
-package com.ssg.ssg_be.login.application;
-
-import com.ssg.config.BaseException;
-import com.ssg.ssg_be.login.domain.LoginDtoReq;
-
-public interface SellerLoginService {
-    String sellerLogin(LoginDtoReq loginDtoReq) throws BaseException;
-}
