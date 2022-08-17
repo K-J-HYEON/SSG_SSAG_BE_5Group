bd235698920f6d69e93b20d5327cddce923c4b99
안대현 <gch05610@gmail.com>
2022-08-17 14:17:23 +0900

    Update : 필터에 CORS 관련 코드 추가

--- src/main/java/com/ssg/ssg_be/SsgBeApplication.java
+++ src/main/java/com/ssg/ssg_be/SsgBeApplication.java
@@ -15,14 +15,4 @@ public class SsgBeApplication {
 		SpringApplication.run(SsgBeApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
-
 }
