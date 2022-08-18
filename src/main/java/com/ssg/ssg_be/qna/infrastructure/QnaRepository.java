895ff7d26564d40b7843029bcde9a94f1d193cca
K-J-HYEON <kjh950601@naver.com>
2022-08-18 15:43:32 +0900

    revise

--- src/main/java/com/ssg/ssg_be/qna/infrastructure/QnaRepository.java
+++ src/main/java/com/ssg/ssg_be/qna/infrastructure/QnaRepository.java
@@ -1,19 +1,14 @@
 package com.ssg.ssg_be.qna.infrastructure;
 
-import com.ssg.config.BaseException;
-import com.ssg.config.BaseResponse;
 import com.ssg.ssg_be.qna.domain.Qna;
 import com.ssg.ssg_be.qna.domain.QnaDtoRes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Repository
 public interface QnaRepository extends JpaRepository<Qna, Long> {
-    List<QnaDtoRes> findByProductProductId(Long productId);
-    boolean existByQna_QnaId(Long qnaId);
+    List<QnaDtoRes> findByProductProductId(Long productId);;
     List<QnaDtoRes> findByUserUserId(Long userId);
 }
