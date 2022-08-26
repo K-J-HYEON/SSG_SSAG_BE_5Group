5dd7d828068c1b16e4773a07323c426538f4de87
K-J-HYEON <kjh950601@naver.com>
2022-08-26 20:33:35 +0900

    pageable

--- src/main/java/com/ssg/ssg_be/category/infrastructure/CategoryConnRepository.java
+++ src/main/java/com/ssg/ssg_be/category/infrastructure/CategoryConnRepository.java
@@ -2,20 +2,18 @@ package com.ssg.ssg_be.category.infrastructure;
 
 import com.ssg.ssg_be.category.domain.CategoryConn;
 import com.ssg.ssg_be.product.domain.CategoryProductDtoRes;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface CategoryConnRepository extends JpaRepository<CategoryConn, Long> {
 
-    Page<CategoryProductDtoRes> findAllBy(Pageable pageable);
-    Page<CategoryProductDtoRes> findByMediumCategoryId(Long mediumCategoryId, Pageable pageable);
-    Page<CategoryProductDtoRes> findBySmallCategorySmallCategoryId(Long smallCategoryId, Pageable pageable);
-    Page<CategoryProductDtoRes> findByLargeCategoryId(Long largeCategoryId, Pageable pageable);
-    Page<CategoryProductDtoRes> findByProductNameContains(String searchWord, Pageable pageable);
+    Slice<CategoryProductDtoRes> findAllBy(Pageable pageable);
+    Slice<CategoryProductDtoRes> findByMediumCategoryId(Long mediumCategoryId, Pageable pageable);
+    Slice<CategoryProductDtoRes> findBySmallCategorySmallCategoryId(Long smallCategoryId, Pageable pageable);
+    Slice<CategoryProductDtoRes> findByLargeCategoryId(Long largeCategoryId, Pageable pageable);
+    Slice<CategoryProductDtoRes> findByProductNameContains(String searchWord, Pageable pageable);
 
 }
--- src/main/java/com/ssg/ssg_be/product/application/ProductServiceImpl.java
+++ src/main/java/com/ssg/ssg_be/product/application/ProductServiceImpl.java
@@ -30,6 +30,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -292,7 +293,7 @@ public class ProductServiceImpl implements ProductService {
         }
     }
 
-    public List<ProductDtoRes> retrieveProductAndReview(Page<CategoryProductDtoRes> products, Long userId) {
+    public List<ProductDtoRes> retrieveProductAndReview(Slice<CategoryProductDtoRes> products, Long userId) {
 
         List<ReviewTotalDto> reviewTotalDtos = new ArrayList<>();
         List<WishDto> wishDtos = new ArrayList<>();
