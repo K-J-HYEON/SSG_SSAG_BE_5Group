c3c0a033aca87e8002963e0093e0c0de2f0592ca
K-J-HYEON <kjh950601@naver.com>
2022-08-31 11:42:54 +0900

    feature/s3

--- src/main/java/com/ssg/utils/s3/S3UploadDtoReq.java
+++ src/main/java/com/ssg/utils/s3/S3UploadDtoReq.java
@@ -1,8 +1,12 @@
 package com.ssg.utils.s3;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
+@Builder
 public class S3UploadDtoReq {
 
     private String saveName;
